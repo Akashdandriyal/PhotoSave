6b9db515e057b2f7e258eebf05792991ab02aba2
Akash Dandriyal <akash.d1409@gmail.com>
2021-02-20 23:04:02 +0530

    added procfile

--- app.js
+++ app.js
@@ -302,7 +302,7 @@ app.use((req, res, next) => {
     });
 });
 
-let port = 3000||process.env.port;
+let port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
